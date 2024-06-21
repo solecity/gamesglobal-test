f40c8d10f4afe17e9ab1447922fe8866dc3ae911
MarianaM <marianaccmoreira@gmail.com>
2024-06-22 01:05:33 +0200

    get operator mock data

--- src/pages/operators/index.tsx
+++ src/pages/operators/index.tsx
@@ -1,5 +1,64 @@
-import { Layout } from "../../components";
+import { useEffect, useState } from "react";
+import { Layout, Form, Button } from "../../components";
+
+import data from "../../data.json";
+
+import styles from "./index.module.css";
+
+export type Operator = {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  gameName: string;
+  approvalStatus: number;
+};
 
 export default function Operators() {
-  return <Layout>operators</Layout>;
+  const [operators, setOperators] = useState<Operator[]>([]);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [modalType, setModalType] = useState<string>("");
+
+  const getOperators = async () => {
+    try {
+      const { operators } = await Promise.resolve(data);
+
+      setOperators(operators);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleForm = (type: string) => {
+    setOpenModal(!openModal);
+    setModalType(type);
+  };
+
+  useEffect(() => {
+    getOperators();
+  }, []);
+
+  return (
+    <Layout>
+      <h2>Operators</h2>
+
+      <Button
+        type="button"
+        variant="primary"
+        label="Add"
+        action={() => handleForm("add")}
+      />
+
+      {openModal && (
+        <Form type={modalType} state={openModal} setState={setOpenModal} />
+      )}
+
+      <div className={styles.list}>
+        {operators.map((op: Operator) => (
+          <span>
+            {op.firstName} {op.lastName}{" "}
+          </span>
+        ))}
+      </div>
+    </Layout>
+  );
 }
